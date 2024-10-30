e9ce2785da4ae7401e1f03be8ce923ec71a94d17
Pedro Rabelo <pnarabelo@gmail.com>
2024-10-30 18:10:12 -0300

    userModel

---
+++ backend/src/models/userModel.js
@@ -0,0 +1,49 @@
+const Sequelize = require('sequelize');
+const database = require('../db');
+const AutomationModel = require('./automationModel');
+const MonitorModel = require('./monitorModel');
+const OrderTemplateModel = require('./orderTemplateModel');
+const WithdrawTemplateModel = require('./withdrawTemplateModel');
+const FavoriteSymbolModel = require('./favoriteSymbolModel');
+const LimitModel = require('./limitModel');
+const WebHookModel = require('./webHookModel');
+
+const userModel = database.define('user', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  name: Sequelize.STRING,
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  limitId: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  phone: Sequelize.STRING,
+  accessKey: Sequelize.STRING,
+  secretKey: Sequelize.STRING,
+  telegramChat: Sequelize.STRING,
+  isActive: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+    defaultValue: false
+  },
+  createdAt: Sequelize.DATE,
+  updatedAt: Sequelize.DATE
+}, {
+  indexes: [{
+    unique: true,
+    fields: ['email']
+  }]
+})
+
+module.exports = userModel
